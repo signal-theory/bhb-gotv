1c3b16d3bc11fd222d684b894d40f8ff19281d43
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-08-19 11:53:19 -0500

    scrollable navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,11 @@ const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <div className="container">
-          <div className="navbar-wrapper">
-            <div className="links-wrapper">
-              <button onClick={() => scrollTo("#work")} onKeyDown={() => scrollTo("#work")}>VOTING IN MISSOURI</button>
-              <button onClick={() => scrollTo("#about")} onKeyDown={() => scrollTo("#about")}>VOTING IN KANSAS</button>
-              <button onClick={() => scrollTo("#contact")} onKeyDown={() => scrollTo("#contact")}>FAQS</button>
-              <button onClick={() => scrollTo("#contact")} onKeyDown={() => scrollTo("#contact")}>BHB OUTREACH</button>
-            </div>
-          </div>
+        <div className="navbar-wrapper">
+          <button onClick={() => scrollTo("#work")} onKeyDown={() => scrollTo("#work")}>VOTING IN MISSOURI</button>
+          <button onClick={() => scrollTo("#about")} onKeyDown={() => scrollTo("#about")}>VOTING IN KANSAS</button>
+          <button onClick={() => scrollTo("#contact")} onKeyDown={() => scrollTo("#contact")}>FAQS</button>
+          <button onClick={() => scrollTo("#contact")} onKeyDown={() => scrollTo("#contact")}>BHB OUTREACH</button>
         </div>
       </div>
       <div className="countdown-sticker">
