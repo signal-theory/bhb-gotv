536217b4b60eab605970030b2a3056be8512c4e3
Beth Wickerson <beth.wickerson@signaltheory.com>
2022-11-30 12:56:04 -0600

    remove nav countdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,6 @@
 import React from "react"
 import scrollTo from "gatsby-plugin-smoothscroll"
-import { Fade } from "react-awesome-reveal"
 const Navbar = (props) => {
-  // COUNTDOWN
-  const countDownDate = new Date("Nov 8, 2022 7:00:00").getTime();
-  // Get today's date and time
-  const now = new Date().getTime();
-  // Find the distance between now and the count down date
-  const distance = countDownDate - now;
-  // Time calculations for days, hours, minutes and seconds
-  const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
-  const daysLeft = days < 10 ? "0" + days : days;
 
   return (
     <>
@@ -23,12 +13,6 @@ const Navbar = (props) => {
           <button onClick={() => scrollTo("#outreach")} onKeyDown={() => scrollTo("#outreach")}>BHB OUTREACH</button>
         </div>
       </div>
-      <Fade style={{ position: 'fixed', zIndex: 100000 }}>
-        <div className="countdown-sticker">
-          <h2>{daysLeft}</h2>
-          <span>DAYS UNTIL ELECTION</span>
-        </div>
-      </Fade>
     </>
   )
 }
